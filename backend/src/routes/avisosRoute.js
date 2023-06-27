7877a8049fe5699014c1732b376b80833cf16cfc
Leo <101261674+Bene-noob@users.noreply.github.com>
2023-06-27 10:58:02 +0100

    Delete backend directory

--- backend/src/routes/avisosRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-//importar os controladores
-const AvisosController = require('../controllers/AvisosController')
-
-router.get('/list' ,AvisosController.list);
-
-module.exports = router;
