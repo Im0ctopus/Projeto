c22fe64b3afc4be0677e335d0728248d0b0f1c52
Demolition <128087367+JohnnCore@users.noreply.github.com>
2023-07-03 16:58:50 +0100

    Update ReunioesIdeiaEdit.js

--- frontend/src/view/Backoffice/Ideias/Reunioes/ReunioesIdeiaEdit.js
+++ frontend/src/view/Backoffice/Ideias/Reunioes/ReunioesIdeiaEdit.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from '../../../auth.header'
 
 import React, { useEffect, useState } from "react";
-import Swal from 'sweetalert2/dist/sweetalert2.js'
+import Swal from 'sweetalert2/dist/sweetalert2.js' 
 import { useNavigate, useParams, Link } from 'react-router-dom';
 import 'sweetalert2/src/sweetalert2.scss'
 import { SyncLoader } from 'react-spinners';
